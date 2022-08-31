f79991f6c29e758c88e24fb61aa9e3cf486aaa35
katredford <katharine.redford@gmail.com>
2022-08-31 17:03:01 -0500

    can now display single thought and reactions

--- section 3/MERN-steps/server/server.js
+++ section 3/MERN-steps/server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 //? STEP-7
 //? import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -24,7 +25,16 @@ app.use(express.json());
 const startApolloServer = async (typeDefs, resolvers) => {
 await server.start();
 //? integrate our Apollo server with the Express application as middleware
-server.applyMiddleware({ app });
+  server.applyMiddleware({ app });
+  
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+}); 
 
 db.once('open', () => {
     app.listen(PORT, () => {
